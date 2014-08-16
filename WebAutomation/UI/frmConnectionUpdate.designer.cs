08dd7405f9f0c82f26deb068e7f9c8a2c57103c3
Dinh Cong Thang <dinhcongthang113@hotmail.com>
2014-08-16 19:10:15 +0700

    Change to using (Gecko WebBrowser) instead of (WebBrowser)
    + ReStructure project using Gecko Web Browser.
    + Add Downloader panel and function to download file (using MyDownloader
    -
    http://www.codeproject.com/Articles/21053/MyDownloader-A-Multi-thread-C-Segmented-Download-M

---
+++ WebAutomation/UI/frmConnectionUpdate.designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WebAutomation
+{
+    partial class frmConnectionUpdate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblServer = new System.Windows.Forms.Label();
+            this.lblDatabase = new System.Windows.Forms.Label();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbxName = new System.Windows.Forms.TextBox();
+            this.tbxServer = new System.Windows.Forms.TextBox();
+            this.tbxDatabase = new System.Windows.Forms.TextBox();
+            this.tbxUser = new System.Windows.Forms.TextBox();
+            this.tbxPassword = new System.Windows.Forms.TextBox();
+            this.tbxProvider = new System.Windows.Forms.TextBox();
+            this.lblProvider = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(16, 27);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(35, 13);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Name";
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Location = new System.Drawing.Point(16, 59);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(38, 13);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "Server";
+            // 
+            // lblDatabase
+            // 
+            this.lblDatabase.AutoSize = true;
+            this.lblDatabase.Location = new System.Drawing.Point(16, 97);
+            this.lblDatabase.Name = "lblDatabase";
+            this.lblDatabase.Size = new System.Drawing.Size(53, 13);
+            this.lblDatabase.TabIndex = 0;
+            this.lblDatabase.Text = "Database";
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(16, 131);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(29, 13);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "User";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(16, 161);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "Password";
+            // 
+            // tbxName
+            // 
+            this.tbxName.Location = new System.Drawing.Point(104, 24);
+            this.tbxName.Name = "tbxName";
+            this.tbxName.Size = new System.Drawing.Size(168, 20);
+            this.tbxName.TabIndex = 1;
+            // 
+            // tbxServer
+            // 
+            this.tbxServer.Location = new System.Drawing.Point(104, 56);
+            this.tbxServer.Name = "tbxServer";
+            this.tbxServer.Size = new System.Drawing.Size(168, 20);
+            this.tbxServer.TabIndex = 2;
+            // 
+            // tbxDatabase
+            // 
+            this.tbxDatabase.Location = new System.Drawing.Point(104, 94);
+            this.tbxDatabase.Name = "tbxDatabase";
+            this.tbxDatabase.Size = new System.Drawing.Size(168, 20);
+            this.tbxDatabase.TabIndex = 3;
+            // 
+            // tbxUser
+            // 
+            this.tbxUser.Location = new System.Drawing.Point(104, 128);
+            this.tbxUser.Name = "tbxUser";
+            this.tbxUser.Size = new System.Drawing.Size(168, 20);
+            this.tbxUser.TabIndex = 4;
+            // 
+            // tbxPassword
+            // 
+            this.tbxPassword.Location = new System.Drawing.Point(104, 158);
+            this.tbxPassword.Name = "tbxPassword";
+            this.tbxPassword.PasswordChar = '*';
+            this.tbxPassword.Size = new System.Drawing.Size(168, 20);
+            this.tbxPassword.TabIndex = 5;
+            this.tbxPassword.UseSystemPasswordChar = true;
+            // 
+            // tbxProvider
+            // 
+            this.tbxProvider.Location = new System.Drawing.Point(104, 193);
+            this.tbxProvider.Name = "tbxProvider";
+            this.tbxProvider.Size = new System.Drawing.Size(168, 20);
+            this.tbxProvider.TabIndex = 6;
+            // 
+            // lblProvider
+            // 
+            this.lblProvider.AutoSize = true;
+            this.lblProvider.Location = new System.Drawing.Point(16, 196);
+            this.lblProvider.Name = "lblProvider";
+            this.lblProvider.Size = new System.Drawing.Size(46, 13);
+            this.lblProvider.TabIndex = 0;
+            this.lblProvider.Text = "Provider";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(104, 227);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 7;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(185, 227);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // frmConnectionUpdate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.tbxProvider);
+            this.Controls.Add(this.tbxPassword);
+            this.Controls.Add(this.tbxUser);
+            this.Controls.Add(this.tbxDatabase);
+            this.Controls.Add(this.tbxServer);
+            this.Controls.Add(this.tbxName);
+            this.Controls.Add(this.lblProvider);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.lblDatabase);
+            this.Controls.Add(this.lblServer);
+            this.Controls.Add(this.lblName);
+            this.Name = "frmConnectionUpdate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connection";
+            this.Load += new System.EventHandler(this.frmConnectionUpdate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.Label lblDatabase;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbxName;
+        private System.Windows.Forms.TextBox tbxServer;
+        private System.Windows.Forms.TextBox tbxDatabase;
+        private System.Windows.Forms.TextBox tbxUser;
+        private System.Windows.Forms.TextBox tbxPassword;
+        private System.Windows.Forms.TextBox tbxProvider;
+        private System.Windows.Forms.Label lblProvider;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
